375780cfe11028658fd2ac5509461f2e1febdaf5
Irineu Junior <irineu@fmd.ag>
2020-01-06 14:33:45 -0300

    feat(error): adiciona bag de error

--- src/resources/views/includes/form-open.blade.php
+++ src/resources/views/includes/form-open.blade.php
@@ -3,7 +3,11 @@
     $method = strtolower($__data[1]);
     $attributes = $__data[2] ?? [];
 
-    $attributes['class'] = 'needs-validation ' . ((count($errors) > 0) ? 'was-validated ' : '') . (($attributes['class']) ?? '');
+    $bag = $attributes['bag'] ?? 'admix';
+    unset($attributes['bag']);
+
+    $attributes['class'] = 'needs-validation ' . (($errors->{$bag}->any()) ? 'was-validated ' : '') . (($attributes['class']) ?? '');
+    $attributes['novalidade'] = 'novalidate';
 
     $methodForm = (in_array($method, ['patch', 'put', 'delete', 'post'])) ? 'post' : 'get';
 @endphp
