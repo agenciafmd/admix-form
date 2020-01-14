389c993d37ad25f97f498c5330b7ee38f6d5e94d
Irineu Junior <irineu@fmd.ag>
2020-01-14 13:50:07 -0300

    feat(form): troca {!! por {{

--- src/resources/views/components/form/open.blade.php
+++ src/resources/views/components/form/open.blade.php
@@ -1,9 +1,9 @@
 @if(request()->is('*/create') || request()->is('*/create/*'))
-    {!! Form::open(['url' => $params['create'],
+    {{ Form::open(['url' => $params['create'],
         'class' => 'mb-0 card-list-group card needs-validation' . ((count($errors) > 0) ? ' was-validated' : ''),
-        'novalidate' => true, 'id' => 'formCrud', 'files' => true ]) !!}
+        'novalidate' => true, 'id' => 'formCrud', 'files' => true ]) }}
 @else
-    {!! Form::model($params['model'], ['url' => $params['update'], 'method' => 'PUT',
+    {{ Form::model($params['model'], ['url' => $params['update'], 'method' => 'PUT',
         'class' => 'mb-0 card-list-group card needs-validation' . ((count($errors) > 0) ? ' was-validated' : ''),
-        'novalidate' => true, 'id' => 'formCrud', 'files' => true]) !!}
+        'novalidate' => true, 'id' => 'formCrud', 'files' => true]) }}
 @endif
